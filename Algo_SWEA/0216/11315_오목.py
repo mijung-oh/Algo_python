7353f775f4622441bba97df50e923653fed3a780
mijung-oh <ks03102@naver.com>
2021-02-22 00:47:38 +0900

    ADD 0216 algo

--- Algo_SWEA/0216/11315_오목.py
+++ Algo_SWEA/0216/11315_오목.py
@@ -1,45 +1,52 @@
+import sys
+sys.stdin = open('오목판정.txt', 'r')
+
 T = int(input())
 
 def isCross(lst):
     # 정방향 a 반대방향 b
     cross_a = 0
     cross_b = 0
-    l = len(lst)
-    for i in range(l):
-        for j in range(l):
-            if i == j and lst[i][j] == 'o':
-                cross_a += 1
-            elif i+j == l-1 and lst[i][j] == 'o':
-                print(i, j)
-                cross_b += 1
-    if cross_a == l or cross_b == l:
+    for i in range(len(lst)-4):
+        for j in range(len(lst)):
+            if lst[i][j] == 'o' and lst[i+1][j+1] == 'o' and lst[i+2][j+2] == 'o' and lst[i+3][j+3] == 'o' and lst[i+4][j+4] == 'o':
+                return True
+            # if lst[i][len(lst)-j-1] == 'o' and lst[i+1][len(lst)-j-2] == 'o' and lst[i+2][len(lst)-j-3] == 'o' and lst[i+3][len(lst)-j-4] == 'o' and lst[i+4][len(lst)-j-5] == 'o':
+            #     return True
+            if lst[i][len(lst)-j] == 'o' and lst[i+1][len(lst)-j-1] == 'o' and lst[i+2][len(lst)-j-2] == 'o' and lst[i+3][len(lst)-j-3] == 'o' and lst[i+4][len(lst)-j-4] == 'o':
+                return True
+
+
+def isRow(lst):
+    check = 0
+    for i in range(len(lst)):
+        for j in range(len(lst)):
+            if j+4 < len(lst) and lst[i][j] == 'o' and lst[i][j+1] == 'o' and lst[i][j+2] == 'o' and lst[i][j+3] == 'o' and lst[i][j+4] == 'o':
+                check = 1
+                break
+    if check:
         return True
     return False
 
-def isRow(lst):
-    l = len(lst)
-    for i in range(l):
-        if lst[i].count('o') == l:
-            return True
+def isCol(lst):
+    check = 0
+    for i in range(len(lst)):
+        for j in range(len(lst)):
+            if j+4 < len(lst) and lst[j][i] == 'o' and lst[j+1][i] == 'o' and lst[j+2][i] == 'o' and lst[j+3][i] == 'o' and lst[j+4][i] == 'o':
+                check = 1
+                break
+    if check:
+        return True
     return False
 
-# def isCol(lst):
-#     l = len(lst)
-#     t=[]
-#     for i in range(l):
-#         t.append([k[i] for k in a])
-#     for i in range(l):
-#         if t[i].count('o') == l:
-#             return True
-#     return False
-
-for t in range(1, T+1):
+for tc in range(1, T+1):
     N = int(input())
     lst = []
     for i in range(N):
-        lst.append(list(input().split()))
-    
+        t = list(input())
+        lst.append(t)
+    # print(lst)
     if isCross(lst) or isCol(lst) or isRow(lst):
-        print(f'#{t} Yes')
+        print('#{} YES'.format(tc))
     else: 
-        print(f'#{t} No')
+        print('#{} NO'.format(tc))
