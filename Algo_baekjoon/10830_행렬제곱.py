59bf99a4662316ce65cc1a48b5bb00b0bf38a876
mijung-oh <ks03102@naver.com>
2021-03-22 01:02:03 +0900

    ADD 0322 algo

---
+++ Algo_baekjoon/10830_행렬제곱.py
@@ -0,0 +1,32 @@
+N, B = map(int, input().split())
+
+BRD = []
+for n in range(N):
+    BRD.append(list(map(int, input().split())))
+
+def mulList(a, b):
+    result = 0
+    for i in range(len(a)):
+        result += a[i] * b[i]
+    return result
+
+
+def mulMatrix(a, b):
+    result = []
+    for i in range(len(a)):
+        for j in range(len(a)):
+            r = a[i]
+            c = [k[j] for k in b]
+            result.append(mulList(r, c))
+    return result
+
+count = 1
+total = BRD
+while count != B:
+    if count * 2 > B:
+        total = mulMatrix(total, BRD)
+        count += 1
+    else:
+        total = mulMatrix(total, total)
+        count *= 2
+print(total)
