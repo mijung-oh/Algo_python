dbb8c99e2d2270f9e29bf22ffb15689cd2ff5105
mijung-oh <ks03102@naver.com>
2021-07-23 00:43:29 +0900

    Add 0723 algo

---
+++ Algo_baekjoon/1120_문자열.py
@@ -0,0 +1,39 @@
+A, B = input().split()
+# print(A, B)
+
+# def dfs(a):
+#     global B, min_gap
+#     gap = 0
+
+#     if len(a) > len(B):
+#         return
+
+#     if len(a) == len(B):
+#         for i in range(len(a)):
+#             if a[i] != B[i]:
+#                 gap += 1
+#         if min_gap > gap:
+#             min_gap = gap
+#         return
+        
+#     alphabet = [chr(i) for i in range(97,123)] ## a ~ z
+
+#     # A 앞에 알파벳 추가
+#     for i in alphabet:
+#         dfs(a+i)
+#         dfs(i+a)
+
+min_gap = 9999
+# dfs(A)
+for i in range(len(B) - len(A) + 1):
+    gap = 0
+    for j in range(len(A)):
+        if A[j] != B[i+j]:
+            gap += 1
+
+    if min_gap > gap:
+        min_gap = gap
+
+
+    
+print(min_gap)
--- Algo_baekjoon/1436_영화감독.py
+++ Algo_baekjoon/1436_영화감독.py
@@ -8,26 +8,23 @@ one = [i for i in range(0, 10)]
 # two = [i for i in range(11, 100)]
 
 pre_len = 3
-for t in range(30):
-    for i in sort_list:
-        if len(str(i)) > pre_len:
-            continue
-        sub_list = []
+test = [default]
+# for t in range(2):
+while len(sort_list) < 10000:
+    sub_list = []
+    for i in test:
         for j in one:
             sub_list.append(int(str(i) + str(j)))
             sub_list.append(int(str(j) + str(i)))
-        # sublist 중복 제거
-    sort_list += (sub_list)
-    pre_len += 1
-    # sort_list = list(set(sort_list))
-    # print(sort_list)
-    # print(len(sort_list))
-    # if len(sort_list) > 1000:
-    #     break
+    test = sub_list[:]
+    sort_list += sub_list
+    if len(sort_list) >= 10000:
+        break
 
-# print(sort_list)
-print(sorted(sort_list))
-print('길이:', len(sort_list))
+sort_list = list(set(sort_list))
+# print(sorted(sort_list))
+print(sorted(sort_list)[N-1])
+# print('길이:', len(sort_list))
 # a = ['1', '01', '2', '02']
 # print(sorted(a))
 # print(sorted(sort_list)[N-1])
