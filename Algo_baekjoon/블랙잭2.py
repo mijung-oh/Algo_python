b543c9bcfb06d3550c028d0875c53ed42a3c06a6
mijung-oh <ks03102@naver.com>
2021-09-08 00:00:38 +0900

    Add 0908 algo commit message modify

--- Algo_baekjoon/블랙잭2.py
+++ Algo_baekjoon/블랙잭2.py
@@ -2,16 +2,12 @@ N, M = map(int, input().split())
 
 numbers = list(map(int, input().split()))
 
-# numbers.sort()
 max_sum = -1
 sub_sum = 0
 
-for i in range(len(numbers)-1, -1, -1):
-    # i: 9 8 7 6 5
-    for j in range(i-1, -1, -1):
-        # j: 8 7 6 5
-        for k in range(j-1, -1, -1):
-            # k: 7 6 5
+for i in range(len(numbers)):
+    for j in range(i+1, len(numbers)):
+        for k in range(j+1, len(numbers)):
             sub_sum = numbers[i] + numbers[j] + numbers[k]
 
             if sub_sum <= M:
