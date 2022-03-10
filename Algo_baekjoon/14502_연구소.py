e20d1f02664beec6ff19f3f67fe00bf00b9f51bc
mijung-oh <ks03102@naver.com>
2022-03-10 21:02:00 +0900

    Add 0310 algo - 14502 연구소 브루트포스

--- Algo_baekjoon/14502_연구소.py
+++ Algo_baekjoon/14502_연구소.py
@@ -1,4 +1,5 @@
 import copy
+from collections import deque
 
 
 N, M = map(int, input().split())
@@ -13,33 +14,35 @@ max_safezone = 0
 def safezone(brd):
     global N, M, max_safezone
     cnt = 0
-    afterBrd = copy.deepcopy(brd)
-    
-    # print("===============================")
-    # for i in range(len(brd)):
-    #     print(*brd[i])
+    after_brd = copy.deepcopy(brd)
+    # print("================ before =================")
+    # for i in range(len(after_brd)):
+    #     print(*after_brd[i])
 
+    q = deque()
+    # 2인 곳을 q에 넣는다.
     for i in range(N):
         for j in range(M):
-            if afterBrd[i][j] == 2 :
-                # 상하좌우로 0인 부분들은 다 1로 설정
-                for d in [(1,0), (0,1), (-1,0), (0,-1)]:
-                    nr = i + d[0]
-                    nc = j + d[1]
-                    while 0 <= nr < N and 0 <= nc < M and afterBrd[nr][nc] == 0:
-                        afterBrd[nr][nc] = 1
-                        nr += d[0]
-                        nc += d[1]
+            if after_brd[i][j] == 2:
+                q.append((i,j))
+    while q:
+        cur_r, cur_c = q.popleft()
+
+        for d in [(1,0), (0,1), (-1,0), (0,-1)]:
+            nr = cur_r + d[0]
+            nc = cur_c + d[1]
+            if 0 <= nr < N and 0 <= nc < M and after_brd[nr][nc] == 0:
+                after_brd[nr][nc] = 2
+                q.append((nr, nc))
+
+    # print("==============after ===================")
+    # for i in range(len(after_brd)):
+    #     print(*after_brd[i])
     # 남은 0 개수 세기
     for i in range(N):
         for j in range(M):
-            if afterBrd[i][j] == 0:
+            if after_brd[i][j] == 0:
                 cnt += 1
-    
-    if cnt == 31:
-        print("===================================")
-        for i in range(len(brd)):
-            print(*afterBrd[i])
 
     if max_safezone < cnt:
         max_safezone = cnt
@@ -55,9 +58,6 @@ def dfs(brd, cnt):
     for i in range(N):
         for j in range(M):
             if brd[i][j] == 0:
-                if i == 3 and j == 5:
-                    print(cnt, "hihi")
-                # print("cnt : " , cnt , " i, j : ", i, j)
                 brd[i][j] = 1
                 cnt += 1
                 dfs(brd, cnt)
@@ -67,4 +67,4 @@ def dfs(brd, cnt):
 
 
 dfs(BRD, 0)
-print("result : " , max_safezone)
+print(max_safezone)
