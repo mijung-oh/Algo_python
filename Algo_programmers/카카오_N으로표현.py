8cf98a1daba23eac5b95b535302286097dfb128e
mijung-oh <ks03102@naver.com>
2022-03-15 15:35:31 +0900

    Add 0315 algo - 카카오 N으로 표현

---
+++ Algo_programmers/카카오_N으로표현.py
@@ -0,0 +1,40 @@
+from collections import deque
+def solution(N, number):
+    # N으로 계산가능한 모든 것을 해본다.
+
+    maxV = 0
+    # N으로만 표현할 수 있는 값들을 넣는다.
+    q = deque()
+    for i in range(8):
+        v = 0
+        for j in range(i, -1, -1):
+            v += N * (10**j)
+        q.append((v, i+1))
+        if i == 7: maxV = v
+    
+    countV = [10 for _ in range(maxV * N+1)]
+    
+    while q:
+        curV, cnt = q.popleft()
+        print(curV, cnt)
+        countV[curV] = cnt
+
+        # 계산되는 값들을 다 넣는다.
+        # countV의 값이 더 작을 경우에만 초기화시킨다.
+        op = ["+", "-", "//", "*"]
+        for o in op:
+            if o == "+" and countV[curV + N] > cnt+1 and cnt+1 < 9:
+                q.append((curV + N, cnt+1))
+            elif o == "-" and countV[curV - N] > cnt+1 and cnt+1 < 9:
+                q.append((curV - N, cnt+1))
+            elif o == "*" and countV[curV * N] > cnt+1 and cnt+1 < 9:
+                q.append((curV * N, cnt+1))
+            elif o == "//" and countV[curV // N] > cnt+1 and cnt+1 < 9:
+                q.append((curV // N, cnt+1))
+
+
+    print("result: ", countV[number])
+    if countV[number] > 8:
+        return -1
+    return countV[number]
+solution(5, 31168)
