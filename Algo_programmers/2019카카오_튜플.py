75d199be2454704e30917d542e544e295fc8fc8d
mijung-oh <ks03102@naver.com>
2022-03-18 18:43:38 +0900

    Add 0318 algo - 2019 카카오 튜플 정규표현식

--- Algo_programmers/2019카카오_튜플.py
+++ Algo_programmers/2019카카오_튜플.py
@@ -1,27 +1,37 @@
+# def solution(s):
+#     answer = []
+#     tuples = []
+#     subTuple = []
+#     subString = ""
+
+#     for i in range(1, len(s)-1):
+#         if s[i] != '{' and s[i] != '}' and s[i] != ',':
+#             subString += s[i]
+#         elif s[i] == ',' and s[i+1] != '{':
+#             subTuple.append(subString)
+#             subString = ""
+#         elif s[i] == '}' and i != len(s)-1:
+#             subTuple.append(subString)
+#             subString = ""
+#             tuples.append(subTuple)
+#             subTuple = []
+
+#     tuples = sorted(tuples, key=len)
+#     for t in tuples:
+#         for k in t:
+#             if int(k) in answer:
+#                 continue
+#             answer.append(int(k))
+#     return answer
+
+# 정규표현식
+
 def solution(s):
-    answer = []
-    tuples = []
-    subTuple = []
-    subString = ""
 
-    for i in range(1, len(s)-1):
-        if s[i] != '{' and s[i] != '}' and s[i] != ',':
-            subString += s[i]
-        elif s[i] == ',' and s[i+1] != '{':
-            subTuple.append(subString)
-            subString = ""
-        elif s[i] == '}' and i != len(s)-1:
-            subTuple.append(subString)
-            subString = ""
-            tuples.append(subTuple)
-            subTuple = []
+    s = Counter(re.findall('\d+', s))
+    print(s)
+    return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
 
-    tuples = sorted(tuples, key=len)
-    for t in tuples:
-        for k in t:
-            if int(k) in answer:
-                continue
-            answer.append(int(k))
-    return answer
-
+import re
+from collections import Counter
 solution("{{20,111},{111}}")
