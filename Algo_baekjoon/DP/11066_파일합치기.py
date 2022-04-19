95e9eef1c5e46c46b93edfc6aa1abc89e9801ea8
mijung-oh <ks03102@naver.com>
2022-04-19 15:21:05 +0900

    Add 0419 algo - dp 파일합치기

--- Algo_baekjoon/DP/11066_파일합치기.py
+++ Algo_baekjoon/DP/11066_파일합치기.py
@@ -2,10 +2,6 @@ import sys
 input = sys.stdin.readline
 
 T = int(input())
-
-
-
-
 for t in range(T):
     N = int(input())
     files = list(map(int, input().split()))
@@ -14,27 +10,12 @@ for t in range(T):
     for i in range(N):
         dp[i][i] = 0
 
-    # for i in range(N):
-    #     for j in range(N):
-    #         if i != j:
-    #             d = sum(files[i:j+1])
-    #             minimum = 99999999
-    #             for k in range(j):
-    #                 print(i, j, k, dp[i][k], dp[k+1][j])
-    #                 minimum = min(minimum, dp[i][k] + dp[k+1][j])
-    #                 print(minimum)
-    #             dp[i][j] = d + minimum
-
     for i in range(1, N): # 부분 파일의 길이 1 2 3
-        for j in range(N-1): # 시작점 0 1 2 3
+        for j in range(N-i): # 시작점 0 1 2 3
             # 0 2 = 00 12 / 01 22
             d = sum(files[j:i+j+1])
             minimum = 99999999
             for k in range(j, i+j):
-                print(i, j, k, i+j)
                 minimum = min(minimum, dp[j][k] + dp[k+1][i+j])
-            print(i, j, d, minimum)
             dp[j][j+i] = d + minimum
-    for i in range(len(dp)):
-        print(*dp[i])
-    print(dp[i][N-1])
+    print(dp[0][N-1])
