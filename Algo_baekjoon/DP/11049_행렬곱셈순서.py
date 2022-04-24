fc09d046cb5429b52dd7ef98cec14404b1bba605
mijung-oh <ks03102@naver.com>
2022-04-24 20:05:27 +0900

    Add 0424 algo

--- Algo_baekjoon/DP/11049_행렬곱셈순서.py
+++ Algo_baekjoon/DP/11049_행렬곱셈순서.py
@@ -12,11 +12,10 @@ for n in range(N):
 dp = [[2 ** 31] * N for _ in range(N)]
 for i in range(N):
     dp[i][i] = 0
-
-for i in range(1, N): # 행렬 곱하기 개수
-    for j in range(N-i): # 행렬 곱셈 시작
-        minR = minC = 0
-        for k in range(j, j+i):
+# ABC => 012
+for i in range(1, N): # 행렬 곱하기 개수 i = 2
+    for j in range(N-i): # 행렬 곱셈 시작 j = 0 
+        for k in range(j, j+i): # k = 0, 1
             # 00 12 = A * BC
             # 01 22 = AB * C
             left = (matrix[j][0], matrix[k][1])
@@ -24,4 +23,3 @@ for i in range(1, N): # 행렬 곱하기 개수
             sub_sum = left[0] * left[1] * right[1]
             dp[j][j+i] = min(dp[j][j+i], dp[j][k] + dp[k+1][i+j] + sub_sum)
 print(dp[0][N-1])
-        
