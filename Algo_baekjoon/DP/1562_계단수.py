c521ffd982c1d24d6367bc62166d046a14ee4deb
mijung-oh <ks03102@naver.com>
2022-04-26 17:05:45 +0900

    Add 0426 algo - dp 비트마스킹

--- Algo_baekjoon/DP/1562_계단수.py
+++ Algo_baekjoon/DP/1562_계단수.py
@@ -1,22 +1,43 @@
 N = int(input())
-check = [0 for _ in range(10)] # 0-9 
+# [비트마스킹][현재까지 길이][마지막 숫자]
+dp = [[[-1] * 11 for i in range(101)] for _ in range(1<<11)]
+
 cnt = 0
-def dfs(cur_string, cur):
-    global cnt
-    if len(cur_string) == N:
-        # 만약 0-9 숫자가 모두 check 됐으면
-        if sum(check) == 10:
-            cnt += 1
-        return
-    for i in [-1, 1]:
-        if 0 <= cur + i < 10:
-            check[cur+i] = 1
-            dfs(cur_string + str(cur+i), cur+i)
-            check[cur+i] = 0
-    return
+mod = 1000000000
+def dfs(bit, cur_len, cur_num):
+    global cnt, N, mod
+    if cur_num < 0 or cur_num > 9: return 0
+
+    # 현재까지 길이가 N과 같다면 
+    if cur_len == N:
+        # 비트마스킹이 다 체크 되었다면
+        if bit == (1<<10) - 1:
+            return 1
+        else:
+            return 0
+
+    # dp에 값이 있다면
+    if dp[bit][cur_len][cur_num] != -1:
+        return dp[bit][cur_len][cur_num]
+    dp[bit][cur_len][cur_num] = 0
+    
+    # 현재 숫자가 0이라면
+    if cur_num == 0:
+        dp[bit][cur_len][cur_num] += dfs(bit | (1 << (cur_num+1)), cur_len+1, cur_num+1)
+        dp[bit][cur_len][cur_num] %= mod
+    elif cur_num == 9:
+        dp[bit][cur_len][cur_num] += dfs(bit | (1 << (cur_num-1)), cur_len+1, cur_num-1)
+        dp[bit][cur_len][cur_num] %= mod
+    else:
+        dp[bit][cur_len][cur_num] += dfs(bit | (1 << (cur_num+1)), cur_len+1, cur_num+1)
+        dp[bit][cur_len][cur_num] %= mod
+        dp[bit][cur_len][cur_num] += dfs(bit | (1 << (cur_num-1)), cur_len+1, cur_num-1)
+        dp[bit][cur_len][cur_num] %= mod
+
+    return dp[bit][cur_len][cur_num]
 
 for i in range(1, 10):
-    check[i] = 1
-    dfs(str(i), i)
-    check[i] = 0
+    cnt += dfs(1<<i, 1, i)
+    cnt %= mod
+
 print(cnt)
