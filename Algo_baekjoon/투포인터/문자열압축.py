e170e94b090d6786c84568f84d66b0c403a95228
mijung-oh <ks03102@naver.com>
2022-05-03 21:14:04 +0900

    Add 0503 algo - 문자열 압축

--- Algo_baekjoon/투포인터/문자열압축.py
+++ Algo_baekjoon/투포인터/문자열압축.py
@@ -1,34 +1,28 @@
 def solution(s):
     answer = len(s)
     
-    for i in range(2, len(s)//2+1):
-        sub_result = []
-        st = 0
-        while 1:
-            if st > len(s)-i: break
-            # i 길이만큼 뽑는다.
-            ss = s[st: st+i]
-            print(i, ss)
-            cnt = 0
-            # st+i부터 해당 문자열을 가지고 있는지 확인한다.
-            while 1:
-                if ss == s[st: st+i]:
-                    st += i
-                    cnt += 1
-                else:
-                    print("==", ss, sub_result, st)
-                    sub_result.append(s[st])
-                    st += 1
-                    break
-            # 만약 반복 문자열이 있었다면
-            if cnt:
+    # 압축할 문자열 길이
+    for step in range(1, len(s)-1):
+        sub_string = []
+        prev = s[:step]
+        cnt = 1
+        for j in range(step, len(s), step):
+            if s[j:j+step] == prev:
+                cnt += 1
+            else:
                 if cnt > 1:
-                    sub_result.append(str(cnt))
-                sub_result.append(ss)
-            # 만약 st가 s 바깥을 가리키고 있다면 break
-        print(sub_result)
-        answer = min(answer, len(''.join(sub_result)))
-            
+                    sub_string.append(str(cnt))
+                sub_string.append(prev)
+                cnt = 1
+                prev = s[j:j+step]
+        # 마지막 추가
+        if cnt > 1:
+            sub_string.append(str(cnt))
+        sub_string.append(prev)
+
+        answer = min(answer, len(''.join(sub_string)))
+        print(step, sub_string)
+    print(answer)
             
     return answer
-solution("aabbaccc")
+solution("aababab")
