9f425e05d4c9655eadf646ff11897abf2ce169e0
mijung-oh <ks03102@naver.com>
2022-05-17 23:49:32 +0900

    Add 0517 algo

--- Algo_programmers/멀쩡한사각형.py
+++ Algo_programmers/멀쩡한사각형.py
@@ -2,7 +2,8 @@ def solution(w,h):
     answer = 1
     # 가로: w, 세로: h
     # 가운데를 지나는 직선 함수
-    y = (h*x) / w
+    def slope(x):
+        return (h*x) / w
     
     for i in range()
     return answer
