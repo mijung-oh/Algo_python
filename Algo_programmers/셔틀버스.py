2fc6749a613c441bdb4acd2d960abe6ffaed2f85
mijung-oh <ks03102@naver.com>
2022-05-19 22:57:29 +0900

    Add 0519 algo - 셔틀버스

--- Algo_programmers/셔틀버스.py
+++ Algo_programmers/셔틀버스.py
@@ -1,64 +1,53 @@
 def solution(n, t, m, timetable):
     answer = ''
-    ans_h = 0
-    ans_m = 0
-    # 버스는 n번만 온다. 그러므로 n번이 끝나기 전에 타야 한다.
-    # 버스는 t분 간격으로 오고, m명까지 탈 수 있다.
-    # n번째 버스를 마지막으로 타야 가장 늦게 탄다.
-    # 버스가 도착한 시간
-    cur_h = 9
-    cur_m = 0
 
-    timetable.sort()
-    
-    for bus in range(1, n+1):
-        # 현재 버스를 탈 수 있는 인원을 구한다.
-        possible = []
-        for time in timetable:
-            h, mi = int(time.split(":")[0]), int(time.split(":")[1])
-            if h < cur_h:
-                possible.append(time)
-            elif h == cur_h and mi <= cur_m:
-                possible.append(time)
-        possible.sort()
+    # 시간을 분단위로 바꾼다.
+    # 셔틀 출발 시간
+    cur_bus = 9 * 60
 
-        # 대기열이 있는 경우
-        if possible:
-            # 마지막 버스가 아닐 경우 l명을 pop한다.
-            if bus != n:
-                l = 0
-                if len(possible) > m:
-                    l = m
-                else:
-                    l = len(possible)
-                while l:
-                    timetable.pop(0)
-                    l -= 1
+    # timetable을 분단위로 바꾼다.
+    new_timetable = []
+    for time in timetable:
+        hh, mm = int(time.split(":")[0]), int(time.split(":")[1])
+        new_timetable.append(hh * 60 + mm)
+    new_timetable.sort()
 
-            elif bus == n:
-                print(possible, timetable)
-                # 마지막 버스 인원이 더 많을 경우
-                if len(possible) >= m:
-                    # 가장 늦게 온 사람보다 1분 늦게 온다.
-                    if int(possible[-1].split(":")[1])-1 < 0:
-                        ans_h = int(possible[-1].split(":")[0])-1
-                        ans_m = 59
-                    else:
-                        ans_h = int(possible[-1].split(":")[0])
-                        ans_m = int(possible[-1].split(":")[1])-1
-                        print("!!")
-                    break
-                else:
-                    ans_h = cur_h
-                    ans_m = cur_m
+    # 버스 오는 횟수
+    for i in range(1, n+1):
+        cnt = 0
+        for time in new_timetable:
+            if cur_bus >= time:
+                cnt += 1
 
-        # 현재 시간 업데이트
-        tt = cur_m + t
-        cur_h += tt // 60
-        cur_m += tt % 60
-        print("다음버스: ", cur_h, cur_m)
+        # 마지막 버스가 아닌 경우
+        if i != n:
+            # 버스 출발 시간보다 일찍 대기한 사람 수를 세고, m이하만큼 pop한다.
+            if cnt > m:
+                cnt = m
+            while cnt:
+                new_timetable.pop(0)
+                cnt -= 1
+        # 마지막 버스인 경우
+        else:
+            # 버스 출발 시간보다 대기한 수가 m 이상이면
+            if cnt >= m:
+                # m번째 사람보다 1분 늦게 간다
+                answer = new_timetable[m-1]-1
+                break
+            else:
+                answer = cur_bus
+        # 버스시간 업데이트
+        cur_bus += t
 
-    print(ans_h, ans_m)
+        
+    ans_h = str(answer // 60)
+    ans_m = str(answer % 60)
+    if len(ans_h) == 1:
+        ans_h = '0' + ans_h
+    if len(ans_m) == 1:
+        ans_m = '0' + ans_m
+    answer = ans_h + ":" + ans_m
+    print(answer)
     return answer
 
-solution(1,1,1,["23:59"])
+solution(2,1,2,["09:00", "09:00", "09:00", "09:00"])
