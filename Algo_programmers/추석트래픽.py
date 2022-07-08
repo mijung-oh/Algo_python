43b7a0e967d0b2716a5a05b7c6e10446dfbfe9a7
mijung-oh <ks03102@naver.com>
2022-07-08 15:06:46 +0900

    Add 0708 algo

---
+++ Algo_programmers/추석트래픽.py
@@ -0,0 +1,15 @@
+def solution(lines):
+    answer = 0
+    times = {}
+    for i in range(60*60*24):
+        times[i] = 0
+    
+    for line in lines:
+        day, time, work = line.split()
+        work = float(work[:len(work)-1])
+        # time을 초단위로 변경
+        hour, minute, sec = time.split(":")
+        end = float(sec)*1000
+        st = float(sec) - work * 1000 + 1
+        print(st, end, work)
+    return answer
